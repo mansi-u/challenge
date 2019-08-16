49bff56aec4f7edd72822495e57749da4d355b32
Mansi Upadhyaya <mansi4upadhyaya@gmail.com>
2019-08-16 13:17:51 +0530

    deleting file

--- log.js
+++
@@ -1,6 +0,0 @@
-require('colors')
-module.exports = function(message, critical = false, debug = false) {
-  if (critical) console.error(message.red)
-  else if (debug) console.debug(message.blue)
-  else console.log(message)
-}
